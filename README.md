43a4473a78276980a8d50f3731774a5c7f9e5a47
RajpreetKR <rajpreet@khalsa.com>
2024-01-19 19:35:36 +0000

    date is now shown in its long format rather than its short format

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # daily-planner-app
+
 7th Module Challenge for the Front-End Web Development Bootcamp
+
+https://www.arclab.com/en/kb/htmlcss/display-date-time-javascript-php-ssi.html
+https://www.freecodecamp.org/news/how-to-format-dates-in-javascript/
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
 // shows current date
-let day = new Date();
-document.getElementById("currentDay").innerHTML = day.toLocaleDateString();
+let date = new Date();
+document.getElementById("currentDay").innerHTML = date.toLocaleDateString(
+  "en-uk",
+  { weekday: "long", year: "numeric", month: "long", day: "numeric" } // this part outputs the code as "Friday, 19 January 2024" rather tahn just "1/19/24"
+);
